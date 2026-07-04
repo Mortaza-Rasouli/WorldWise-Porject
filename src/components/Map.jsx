9679d13254d21dc2d68efb5953479b261fa49b10
Mortaza-Rasouli <Mortaza.Rasouli21@gmail.com>
2026-07-04 18:17:57 +0430

    initial commit

---
+++ src/components/Map.jsx
@@ -0,0 +1,23 @@
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={styles.mapContainer} onClick={() => navigate("form")}>
+        <h1>Map</h1>
+        <h1>
+          Position: {lat} ,{lng}
+        </h1>
+        <button onClick={() => setSearchParams({ lat: 21, lng: 42 })}>
+          Change pos
+        </button>
+      </div>
+    </>
+  );
+}
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
-    <>
-      <div>
-        <PageNav />
-        Not found :(
-      </div>
-    </>
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
   );
 }
